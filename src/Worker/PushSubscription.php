88ce209ce42ff1141ae8ceafd17a661a44f7d140
Michael <heluecht@pirati.ca>
2026-09-20 15:45:44 +0000

    TTL is now defined by an existing constant

--- src/Worker/PushSubscription.php
+++ src/Worker/PushSubscription.php
@@ -9,6 +9,7 @@ namespace Friendica\Worker;
 
 use Friendica\Content\Text\BBCode;
 use Friendica\Content\Text\Plaintext;
+use Friendica\Core\Cache\Enum\Duration;
 use Friendica\Database\DBA;
 use Friendica\DI;
 use Friendica\Factory\Api\Mastodon\Notification as NotificationFactory;
@@ -110,7 +111,7 @@ class PushSubscription
 
 		// The 'TTL' has to be a string, else a deprecation warning is thrown by the "guzzlehttp/psr7" library.
 		// @see https://github.com/friendica/friendica/issues/15818#issuecomment-5749515821
-		$report = $webPush->sendOneNotification($push, json_encode($payload), ['urgency' => 'normal', 'TTL' => '86400']);
+		$report = $webPush->sendOneNotification($push, json_encode($payload), ['urgency' => 'normal', 'TTL' => (string) Duration::DAY]);
 
 		$endpoint = $report->getRequest()->getUri()->__toString();
 
